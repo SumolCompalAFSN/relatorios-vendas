8bb9dcecf5d302d018929f4c9366b3cec764578f
SumolCompalAFSN <pedro.gameiro@sumolcompal.pt>
2026-05-21 16:50:15 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -351,9 +351,13 @@ if (mode === 'ATRASO') {
   };
 
   const markAsSent = (key: string) => {
-  const item = results.find(r => `${r.ref3}_${r.email}` === key);
-  if (!item || item.enviado) return;
-  
+  setResults(prev =>
+    prev.map(r =>
+      `${r.ref3}_${r.email}` === key
+        ? { ...r, enviado: true }
+        : r
+    )
+  );
 };
 
   const generateEMLBlob = (item: GroupedData) => {
